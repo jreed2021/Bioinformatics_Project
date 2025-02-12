1fc8da3b62c6da486186a73f8e249aee0ecf6417
jreed2021 <138409867+jreed2021@users.noreply.github.com>
2025-02-12 18:01:11 -0500

    Update graphical_tree.py

--- graphical_tree.py
+++ graphical_tree.py
@@ -1,2 +1,16 @@
+import matplotlib.pyplot as plt
+from Bio import Phylo  
 
+"""
+File name: graphical_tree.py
+Author: Janessa Reed
+Created: 2/11/2024
+Version: 1.0
+
+License: MIT License
+"""
+
+tree = Phylo.read("H5_tree_upgma.nwk", "newick")
+plt.figure(figsize=(10, 8))
+Phylo.draw(tree, do_show=True)
 
